b38bc35a08dd5c2eda6e18dc2afadd143d57b479
Axel Cornelly <acornelly@parsebiosciences.com>
2024-10-10 16:50:00 -0700

    Started creating function to read distance csv file

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ def readPackages(packageFile):
     """ This function reads in a csv file containing package data and appends them
     to the packageHash data structure.
     Args:
-        packageFile (str): A string of the csv file name.
+        packageFile (str): String path to the package csv file.
     """
     with open(packageFile, mode='r') as file:
         csvFile = csv.reader(file)
@@ -19,6 +19,13 @@ def readPackages(packageFile):
             # print(p.toString())
             packageHash.addPackage(p)
 
+# Read in distance data and add to distance 2D list
+def readDistances(distanceFile):
+    """ Reads in a csv file of distances between delivery addresses.
+    Args:
+        distanceFile (str): String path to the distance csv file.
+    """
+    
 
 if __name__ == "__main__":
     readPackages("C950_WGUPS_Program/WGUPS Package File.csv")
