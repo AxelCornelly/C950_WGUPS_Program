5ebccd038522c85c8041ef6bdcf6c4e374ec1048
Axel Cornelly <acornelly@parsebiosciences.com>
2024-10-10 16:09:47 -0700

    Added inline method definitions.

--- PackageClass.py
+++ PackageClass.py
@@ -1,6 +1,6 @@
 class Package:
-    def __init__(self, packageID, address, city, state, zip, deadline, weight, notes):
-        """Class constructor function that defines a Package object
+    def __init__(self, packageID: str, address: str, city:str, state: str, zip:str, deadline: str, weight: str, notes: str):
+        """Class constructor function that defines a Package object.
         Args:
             packageID (int): The ID of the package.
             address (str): The delivery address the package must be delivered to.
@@ -20,34 +20,53 @@ class Package:
         self.weight = weight
         self.notes = notes
     
-    def toString(self):
+    def toString(self) -> str:
+        """ Helper function to display package information in a legible format.
+        Returns:
+            str: String value in the format of [Package ID] | Address | Deadline | Weight | Special Notes
+        """
         return f"[Package {self.packageID}] | Destination: {self.address} {self.city}, {self.state} {self.zip} | Deadline: {self.deadline} | Weighs {self.weight} kilos | Special Notes: {self.notes}"
 
-    def getID(self):
+    def getID(self) -> int:
+        """ Returns the package's ID in the form of an *int*."""
         return int(self.packageID)
     
-    def getAddress(self):
+    def getAddress(self) -> str:
+        """ Returns package's destination address."""
         return self.address
     
-    def getCity(self):
+    def getCity(self) -> str:
+        """ Returns package's destination city."""
         return self.city
     
-    def getState(self):
+    def getState(self) -> str:
+        """ Returns package's destination state."""
         return self.state
     
-    def getZip(self):
+    def getZip(self) -> str:
+        """ Returns package's destination zip code."""
         return self.zip
     
-    def getDeadline(self):
+    def getDeadline(self) -> str:
+        """ Returns package's delivery deadline."""
         return self.deadline
     
-    def getWeight(self):
-        return self.weight
+    def getWeight(self) -> int:
+        """ Returns package's weight in kilos."""
+        return int(self.weight)
     
-    def getNotes(self):
+    def getNotes(self) -> str:
+        """ Returns any special notes attached to the package."""
         return self.notes
     
-    def updateAddress(self, newAddress, newCity, newState, newZip):
+    def updateAddress(self, newAddress: str, newCity: str, newState: str, newZip: str):
+        """ Updates the address of a package.
+        Args:
+            newAddress (str): Package's new destination street address.
+            newCity (str): Package's new destination city.
+            newState(str): Package's new destination state.
+            newZip (str): Package's new destination zipcode.
+        """
         self.address = newAddress
         self.city = newCity
         self.state = newState
