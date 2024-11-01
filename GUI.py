02380d4404097c59a01b426eaee9c1519ccf3a91
AxelCornelly <ace.cornelly@gmail.com>
2024-10-31 20:51:29 -0700

    Begin GUI updates

--- GUI.py
+++ GUI.py
@@ -2,6 +2,14 @@ import tkinter as tk
 import threading
 from DeliverLogic import deliverPackages
 
+def updateTimeEvent(progTime):
+    """ Updates the Program Time label as the program runs.
+    
+    Args:
+        progTime (datetime): A datetime object of the program's current running time.
+    """
+    
+
 def startGUI(truckList):
     """ This method builds and starts the GUI portion of the program.
     
@@ -13,19 +21,26 @@ def startGUI(truckList):
     root = tk.Tk()
     root.title("WGUPS Delivery Program")
 
+    # Create frame to hold Truck labels
+    truckFrame = tk.Frame(master=root, relief=tk.RAISED, borderwidth=3)
+    truckFrame.grid(column=0, row=0)
+
     # Create labels for Truck info
-    trucksLabel = tk.Label(root, text="Delivery Trucks:")
-    truckStatusLabel = tk.Label(root, text="Truck Status")
-    truckMileageLabel = tk.Label(root, text="Truck Mileage")
-    truckLabel1 = tk.Label(root, text= "Truck 1")
-    truck1StatusLabel = tk.Label(root, text="At Hub")
-    truck1MileageLabel = tk.Label(root, text="0.0")
-    truckLabel2 = tk.Label(root, text= "Truck 2")
-    truck2StatusLabel = tk.Label(root, text="At Hub")
-    truck2MileageLabel = tk.Label(root, text="0.0")
-    truckLabel3 = tk.Label(root, text= "Truck 3")
-    truck3StatusLabel = tk.Label(root, text="At Hub")
-    truck3MileageLabel = tk.Label(root, text="0.0")
+    trucksLabel = tk.Label(master=truckFrame, text="Delivery Trucks",)
+    truckStatusLabel = tk.Label(master=truckFrame, text="Truck Status")
+    truckMileageLabel = tk.Label(master=truckFrame, text="Truck Mileage")
+    
+    truckLabel1 = tk.Label(master=truckFrame, text= "Truck 1")
+    truck1StatusLabel = tk.Label(master=truckFrame, text="At Hub")
+    truck1MileageLabel = tk.Label(master=truckFrame, text="0.0")
+    
+    truckLabel2 = tk.Label(master=truckFrame, text= "Truck 2")
+    truck2StatusLabel = tk.Label(master=truckFrame, text="At Hub")
+    truck2MileageLabel = tk.Label(master=truckFrame, text="0.0")
+    
+    truckLabel3 = tk.Label(master=truckFrame, text= "Truck 3")
+    truck3StatusLabel = tk.Label(master=truckFrame, text="At Hub")
+    truck3MileageLabel = tk.Label(master=truckFrame, text="0.0")
 
     trucksLabel.grid(column=0, row=0)
     truckStatusLabel.grid(column=1, row=0)
@@ -44,8 +59,11 @@ def startGUI(truckList):
     truck3MileageLabel.grid(column=2, row=3)
 
     # Label for Program Time
+    timeTitleLabel = tk.Label(root, text="Program Time")
+    timeTitleLabel.grid(column=0, row=1)
+    
     timeLabel = tk.Label(root, text="08:00 AM")
-    timeLabel.grid(column=0, row=5)
+    timeLabel.grid(column=0, row=2)
 
     
     root.mainloop()
