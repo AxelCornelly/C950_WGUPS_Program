3f038c73ae2bb4880d91ae8380c4007226a2dec6
AxelCornelly <ace.cornelly@gmail.com>
2024-11-02 15:33:22 -0700

    Figured out button logic

--- GUI.py
+++ GUI.py
@@ -3,18 +3,14 @@ import tkinter.ttk as ttk
 import threading
 from DeliverLogic import deliverPackages
 
-def control(root: tk.Tk, thread1, thread2):
-    btn = root.nametowidget("controlBtn")
-    if btn["text"] == "Start":
+def control(btn,txt):
+    if btn["text"] == "Start" or btn["text"] == "Resume":
         btn["text"] = "Pause"
-        #thread1.start()
-        #thread2.start()
-    
-    if btn["text"] == "Pause":
+    elif btn["text"] == "Pause":
         btn["text"] = "Resume"
     
-    if btn["text"] == "Resume":
-        btn["text"] = "Pause"
+    rc = root.winfo_children()
+    tlfc =
 
 
 def startGUI(truckList):
@@ -23,23 +19,11 @@ def startGUI(truckList):
     Args:
         truckList (list[Truck]): A list structure containing Truck objects.
     """
-    # Main window
-    root = tk.Tk()
-    root.title("WGUPS Delivery Program")
-    root.geometry("1000x800")
-    root.columnconfigure(1,weight=1,minsize=50)
-    root.rowconfigure(1,weight=1,minsize=50)
-
-    # Create frame to hold Truck Information
-    truckTopLF = tk.LabelFrame(master=root,text="Trucks",labelanchor="n",font=("Times New Roman", 14),background="light gray",bd=3,relief=tk.RAISED)
-    truckTopLF.grid(column=0,row=0,columnspan=3)
-
-
     # Populate Truck Frame with info
-    for i in range(len(truckList)):
+    for index, truck in enumerate(truckList):
         # Create a separate LabelFrame for each truck
-        truckLF = tk.LabelFrame(master=truckTopLF, name=f"t{i+1}Label", text=f"Truck {i+1}",labelanchor="n",font=("Arial",12),bd=3,relief=tk.RAISED)
-        truckLF.grid(column=i,row=0,padx=50,pady=10,sticky="N")
+        truckLF = tk.LabelFrame(master=truckTopLF, name=f"t{index+1}Label", text=f"Truck {index+1}",labelanchor="n",font=("Arial",12),bd=3,relief=tk.RAISED)
+        truckLF.grid(column=index,row=0,padx=50,pady=10,sticky="N")
         
         # Truck Info
         truckStatusLabel = tk.Label(master=truckLF,text="Status: At Hub")
@@ -56,7 +40,7 @@ def startGUI(truckList):
         packageHeader.grid(column=0,row=2)
 
         packageHeaderSepVert = ttk.Separator(master=truckLF,orient="vertical")
-        packageHeaderSepVert.grid(column=1,row=2,rowspan=len(truckList[i].packages)+2,sticky="ns")
+        packageHeaderSepVert.grid(column=1,row=2,rowspan=len(truck.packages)+2,sticky="ns")
 
         packageHeaderSepHor = ttk.Separator(master=truckLF,orient="horizontal")
         packageHeaderSepHor.grid(column=0,row=3,columnspan=3, sticky="ew")
@@ -65,49 +49,57 @@ def startGUI(truckList):
         packageStatusHeader.grid(column=2,row=2)
         
         # Populate the Truck's container with its Packages and their info
-        for index, p in enumerate(truckList[i].packages):
+        for idx, p in enumerate(truck.packages):
             
             packageLabel = tk.Label(master=truckLF,text=f"Package {p.getID()}")
-            packageLabel.grid(column=0,row=index+4)
+            packageLabel.grid(column=0,row=idx+4)
 
             packageStatusLabel = tk.Label(master=truckLF,text=f"{p.getStatus()}")
-            packageStatusLabel.grid(column=2,row=index+4)
-
-    # Create Frame to hold Delivery Updates
-    updatesFrame = tk.Frame(master=root,relief=tk.SUNKEN, borderwidth=3)
-    updatesFrame.grid(column=1,row=1,columnspan=2, rowspan=3)
-
-    # Delivery Updates text area
-    updatesArea = tk.Text(master=updatesFrame,name="updatesArea",width=60,height=20,wrap="word",bg="light gray")
-    updatesArea.grid(column=0,row=0, rowspan=3)
-
-    # Container for Time and Distance Info
-    miscLF = tk.Frame(master=root)
-    miscLF.grid(column=0,row=1)
-
-    # Time Widgets
-    timeLF = tk.LabelFrame(master=miscLF,text="Program Time",pady=10)
-    timeLF.grid(column=0,row=0)
-
-    timeVal = tk.Label(master=timeLF,text="08:00 AM",justify="center")
-    timeVal.grid(column=0,row=0,sticky="nsew")
-
-    # Total Distance Widgets
-    totalDistLF = tk.LabelFrame(master=miscLF,text="Total Distance",pady=10)
-    totalDistLF.grid(column=0,row=1)
-
-    totalDistVal = tk.Label(master=totalDistLF,text="0.0 Miles",justify="center")
-    totalDistVal.grid(column=0,row=0,sticky="ne")
-    
-
-    # Threads to run delivery program
-    thread1 = threading.Thread(target=deliverPackages, args=(root, truckList, truckList[0], "08:00 AM"))
-    thread2 = threading.Thread(target=deliverPackages, args=(root, truckList, truckList[1], "08:00 AM"))
-
-    # Interactive button to Start/Stop/Resume program
-    #controlBtn = tk.Button(master=root,name="controlBtn",text="Start",default="normal",command=control(root, thread1, thread2))
-    #controlBtn.grid(column=2,row=0)
+            packageStatusLabel.grid(column=2,row=idx+4)
     
     
     root.mainloop()
 
+# Main window
+root = tk.Tk()
+root.title("WGUPS Delivery Program")
+root.geometry("1000x800")
+root.columnconfigure(1,weight=1,minsize=50)
+root.rowconfigure(1,weight=1,minsize=50)
+root.configure(bg="dark gray")
+
+# Create frame to hold Truck Information
+truckTopLF = tk.LabelFrame(master=root,text="Trucks",name="truckTopLF",labelanchor="n",font=("Times New Roman", 14),background="light gray",bd=3,relief=tk.RAISED)
+truckTopLF.grid(column=0,row=0,columnspan=3)
+
+# Create Frame to hold Delivery updates and Misc Info.
+updatesFrame = tk.Frame(master=root,name="updatesFrame",relief=tk.SUNKEN, borderwidth=3,bg="dark gray")
+updatesFrame.grid(column=1,row=1,columnspan=2, rowspan=3)
+
+# Delivery Updates text area
+updatesArea = tk.Text(master=updatesFrame,name="updatesArea",width=60,height=20,wrap="word",bg="light green")
+updatesArea.grid(column=1,row=0, rowspan=3)
+
+# Time Widgets
+timeLF = tk.LabelFrame(master=updatesFrame,name="timeLF",text="Program Time",background="light gray",relief=tk.RAISED)
+timeLF.grid(column=0,row=0,padx=5)
+
+timeVal = tk.Label(master=timeLF,text="08:00 AM",name="timeVal",justify="center",background="light gray")
+timeVal.grid(column=0,row=0)
+
+# Total Distance Widgets
+totalDistLF = tk.LabelFrame(master=updatesFrame,text="Total Distance",name="totalDistLF",background="light gray",relief=tk.RAISED)
+totalDistLF.grid(column=0,row=1,sticky="n",padx=5)
+
+totalDistVal = tk.Label(master=totalDistLF,text="0.0 Miles",name="totalDistVal",justify="center",background="light gray")
+totalDistVal.grid(column=0,row=0,sticky="ne")
+
+# Interactive button to Start/Stop/Resume program
+controlBtn = tk.Button(master=updatesFrame,
+                        name="controlBtn",
+                        text="Start",
+                        default="normal",
+                        width=10,
+                        relief=tk.RAISED,
+                        command=lambda: control(controlBtn, "TEXT"))
+controlBtn.grid(column=0,row=2)
