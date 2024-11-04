3e1a05fc63ff2de603bc9b42bac2e71ef15a25be
Axel Cornelly <acornelly@parsebiosciences.com>
2024-11-03 17:29:48 -0800

    Finished GUI layout and start button logic

--- GUI.py
+++ GUI.py
@@ -3,14 +3,14 @@ import tkinter.ttk as ttk
 import threading
 from DeliverLogic import deliverPackages
 
-def control(btn,txt):
+def control(btn,thread1,thread2):
     if btn["text"] == "Start" or btn["text"] == "Resume":
+        if btn["text"] == "Start":
+            thread1.start()
+            thread2.start()
         btn["text"] = "Pause"
     elif btn["text"] == "Pause":
         btn["text"] = "Resume"
-    
-    rc = root.winfo_children()
-    tlfc =
 
 
 def startGUI(truckList):
@@ -21,7 +21,6 @@ def startGUI(truckList):
     """
     # Populate Truck Frame with info
     for index, truck in enumerate(truckList):
-        # Create a separate LabelFrame for each truck
         truckLF = tk.LabelFrame(master=truckTopLF, name=f"t{index+1}Label", text=f"Truck {index+1}",labelanchor="n",font=("Arial",12),bd=3,relief=tk.RAISED)
         truckLF.grid(column=index,row=0,padx=50,pady=10,sticky="N")
         
@@ -57,49 +56,51 @@ def startGUI(truckList):
             packageStatusLabel = tk.Label(master=truckLF,text=f"{p.getStatus()}")
             packageStatusLabel.grid(column=2,row=idx+4)
     
+    # Threads to start deliveries
+    thread1 = threading.Thread(target=deliverPackages,args=(root,truckList,truckList[0],"08:00 AM"))
+    thread2 = threading.Thread(target=deliverPackages,args=(root,truckList,truckList[1],"08:00 AM"))
+
+    # Update Button command
+    controlBtn.config(command=lambda: control(controlBtn,thread1,thread2))
     
     root.mainloop()
 
 # Main window
 root = tk.Tk()
 root.title("WGUPS Delivery Program")
-root.geometry("1000x800")
-root.columnconfigure(1,weight=1,minsize=50)
-root.rowconfigure(1,weight=1,minsize=50)
+root.geometry("1014x825")
 root.configure(bg="dark gray")
 
 # Create frame to hold Truck Information
 truckTopLF = tk.LabelFrame(master=root,text="Trucks",name="truckTopLF",labelanchor="n",font=("Times New Roman", 14),background="light gray",bd=3,relief=tk.RAISED)
-truckTopLF.grid(column=0,row=0,columnspan=3)
-
-# Create Frame to hold Delivery updates and Misc Info.
-updatesFrame = tk.Frame(master=root,name="updatesFrame",relief=tk.SUNKEN, borderwidth=3,bg="dark gray")
-updatesFrame.grid(column=1,row=1,columnspan=2, rowspan=3)
+truckTopLF.grid(column=0,row=0,columnspan=5)
 
 # Delivery Updates text area
-updatesArea = tk.Text(master=updatesFrame,name="updatesArea",width=60,height=20,wrap="word",bg="light green")
-updatesArea.grid(column=1,row=0, rowspan=3)
+scrollbar = tk.Scrollbar(master=root)
+updatesArea = tk.Text(master=root,name="updatesArea",wrap="word",bg="light green",height=18,yscrollcommand=scrollbar.set)
+updatesArea.grid(column=1,row=3,columnspan=3)
+scrollbar.grid(column=4,row=3,sticky="nsw")
+scrollbar.config(command=updatesArea.yview)
 
 # Time Widgets
-timeLF = tk.LabelFrame(master=updatesFrame,name="timeLF",text="Program Time",background="light gray",relief=tk.RAISED)
-timeLF.grid(column=0,row=0,padx=5)
+timeLF = tk.LabelFrame(master=root,name="timeLF",text="Program Time",background="light gray",relief=tk.RAISED)
+timeLF.grid(column=2,row=2)
 
 timeVal = tk.Label(master=timeLF,text="08:00 AM",name="timeVal",justify="center",background="light gray")
 timeVal.grid(column=0,row=0)
 
 # Total Distance Widgets
-totalDistLF = tk.LabelFrame(master=updatesFrame,text="Total Distance",name="totalDistLF",background="light gray",relief=tk.RAISED)
-totalDistLF.grid(column=0,row=1,sticky="n",padx=5)
+totalDistLF = tk.LabelFrame(master=root,text="Total Distance",name="totalDistLF",background="light gray",relief=tk.RAISED)
+totalDistLF.grid(column=3,row=2)
 
 totalDistVal = tk.Label(master=totalDistLF,text="0.0 Miles",name="totalDistVal",justify="center",background="light gray")
-totalDistVal.grid(column=0,row=0,sticky="ne")
+totalDistVal.grid(column=0,row=0)
 
 # Interactive button to Start/Stop/Resume program
-controlBtn = tk.Button(master=updatesFrame,
+controlBtn = tk.Button(master=root,
                         name="controlBtn",
                         text="Start",
                         default="normal",
                         width=10,
-                        relief=tk.RAISED,
-                        command=lambda: control(controlBtn, "TEXT"))
-controlBtn.grid(column=0,row=2)
+                        relief=tk.RAISED)
+controlBtn.grid(column=1,row=2)
