19d4c89e7480b40810c07899d700c5770a941384
Axel Cornelly <acornelly@parsebiosciences.com>
2024-11-06 16:08:02 -0800

    Added function to enter a time to search for packages

--- GUI.py
+++ GUI.py
@@ -1,18 +1,83 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import threading
+import datetime
 from DeliverLogic import deliverPackages
 
 def control(btn,thread1,thread2):
     if btn["text"] == "Start" or btn["text"] == "Resume":
         if btn["text"] == "Start":
+            disableTimeSearch()
             thread1.start()
             thread2.start()
+        elif btn["text"] == "Resume":
+            # Disable our time searching functions
+            disableTimeSearch()
         btn["text"] = "Pause"
     elif btn["text"] == "Pause":
+        # Enable our time searching functions
+        enableTimeSearch()
         btn["text"] = "Resume"
     elif btn["text"] == "Exit":
         root.destroy()
+
+def enableTimeSearch():
+    # Change focus
+    controlBtn.focus()
+
+    # Enable function to enter in a time to see package statuses
+    timeViewBtn.config(state="normal")
+    timeViewEntry.delete(0,tk.END)
+    timeViewEntry.insert(0,"Enter time (H:M AM/PM)")
+    timeViewEntry.config(state="normal")
+    timeViewInfoLabel.grid_forget()
+
+def disableTimeSearch():
+    # change focus
+    controlBtn.focus()
+
+    # Disable our time searching functions
+    timeViewBtn.config(state="disabled")
+    timeViewEntry.delete(0,tk.END)
+    timeViewEntry.insert(0,"Enter time (H:M AM/PM)")
+    timeViewEntry.config(state="readonly")
+    timeViewInfoLabel.config(text="Unable to search while \nprogram is running.")
+    timeViewInfoLabel.grid(column=0,row=2)
+
+def clearEntry(event):
+    if event.widget.get():
+        event.widget.delete(0,tk.END)
+
+def handleTimeViewBtn():
+    # Switch focus to button widget
+    timeViewBtn.focus()
+
+    # Store user's input
+    timeInput = timeViewEntry.get()
+
+    # Validate user input by attempting to format the entry as a datetime
+    try:
+        # This block will execute if the input is a valid time value
+        validTimeInput = datetime.datetime.strptime(timeInput,"%H:%M %p")
+        # Search for string
+        timeLoc = updatesArea.search(timeInput,"0.0",tk.END,exact=False,nocase=True)
+        updatesArea.see(timeLoc)
+        timeViewInfoLabel.grid_forget()
+    except Exception as e:
+        # This block will execute if the input is not valid i.e not entering in a time
+        timeViewInfoLabel.config(text="No packages delivered at that time.")
+        timeViewInfoLabel.grid(column=0,row=2)
+
+def handleEnterKey(event):
+    timeInput = event.widget.get()
+
+    try:
+        validTimeInput = datetime.datetime.strptime(timeInput,"%H:%M %p")
+        timeLoc = updatesArea.search(timeInput,"0.0",tk.END,exact=False,nocase=True)
+        updatesArea.see(timeLoc)
+        print("valid")
+    except Exception as e:
+        print("nope")
 
 def startGUI(truckList):
     """ This method builds and starts the GUI portion of the program.
@@ -63,17 +128,38 @@ def startGUI(truckList):
 
     # Update Button command
     controlBtn.config(command=lambda: control(controlBtn,thread1,thread2))
+
+    disableTimeSearch()
+    timeViewInfoLabel.config(text="Unable to search.\nProgram never started.")
     
     root.mainloop()
 
 # Main window
 root = tk.Tk()
 root.title("WGUPS Delivery Program")
-root.geometry("1014x825")
+root.geometry("1014x900")
 root.configure(bg="dark gray")
+root.columnconfigure(0,weight=1)
+root.rowconfigure(0,weight=1)
+
+"""
+     Cols    0          1              2              3                  4
+Rows   0  [                  truckTopLF(0,0), span 5 col                        ]
+       1
+       2       [controlBtn(1,2)][timeLF(2,2)][totalDistLF(3,2)] [timeViewLF(4,2)]
+       3       [      updatesArea(1,3), span 3 col    ]         [scrollbar (4,3)]
+       4
+"""
 
 # Create frame to hold Truck Information
-truckTopLF = tk.LabelFrame(master=root,text="Trucks",name="truckTopLF",labelanchor="n",font=("Times New Roman", 14),background="light gray",bd=3,relief=tk.RAISED)
+truckTopLF = tk.LabelFrame(master=root,
+                           text="Trucks",
+                           name="truckTopLF",
+                           labelanchor="n",
+                           font=("Times New Roman", 14),
+                           background="light gray",
+                           bd=3,
+                           relief=tk.RAISED)
 truckTopLF.grid(column=0,row=0,columnspan=5)
 
 # Delivery Updates text area
@@ -96,6 +182,27 @@ totalDistLF.grid(column=3,row=2)
 
 totalDistVal = tk.Label(master=totalDistLF,text="0.0 Miles",name="totalDistVal",justify="center",background="light gray")
 totalDistVal.grid(column=0,row=0)
+
+# Time Selector Widget
+timeViewLF = tk.LabelFrame(master=root,text="View package statuses \nat a specific time",name="timeViewLF",background="light gray",relief=tk.RAISED)
+timeViewLF.grid(column=4,row=2,pady=10)
+
+timeViewEntry = tk.Entry(master=timeViewLF,name="timeViewEntry",justify="center",relief=tk.SUNKEN)
+timeViewEntry.bind("<FocusIn>",clearEntry) # Clears entry contents upon gaining focus
+timeViewEntry.bind("<Return>",handleEnterKey)
+timeViewEntry.grid(column=0,row=0,sticky="ew",padx=3)
+
+# Button widget to submit time entry
+timeViewBtn = tk.Button(master=timeViewLF,name="timeViewBtn",text="View Packages",relief=tk.RAISED,command=lambda: handleTimeViewBtn())
+timeViewBtn.grid(column=0,row=1,sticky="ew",padx=3,pady=3)
+
+# Label widget to inform user about time lookup
+timeViewInfoLabel = tk.Label(master=timeViewLF,
+                             name="timeViewInfoLabel",
+                             text="Unable to search.\nProgram not started.",
+                             justify="center",
+                             font=("italicized", 10),bg="light gray",fg="red")
+timeViewInfoLabel.grid(column=0,row=2)
 
 # Interactive button to Start/Stop/Resume program
 controlBtn = tk.Button(master=root,
